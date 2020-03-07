ddb41b4c275c0d5ddceb470c63788819f7372062
Bin WU <wubinben@icloud.com>
2020-03-07 22:59:43 +0800

    eliminate magic number for MOBILE

--- src/main/java/org/example/usecase/Utils.java
+++ src/main/java/org/example/usecase/Utils.java
@@ -1,8 +1,11 @@
 package org.example.usecase;
 
 public class Utils {
+
+    public static final int MOBILE = 11;
+
     static void verifyBeforeAcceptanceForPhoneAndMobile(RuleEntity ruleEntity) throws ValidateException {
-        if (Long.valueOf(CandidateModeEnum.PHONE.getValue()).equals(ruleEntity.getIssueWay()) || ruleEntity.getIssueWay() == 11) {
+        if (Long.valueOf(CandidateModeEnum.PHONE.getValue()).equals(ruleEntity.getIssueWay()) || ruleEntity.getIssueWay() == MOBILE) {
             throw new ValidateException("电话与手机平台不允许此操作!");
         }
     }
